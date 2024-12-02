4417c56acb658581c58a295f4896e8a2022bbde3
Gabryel Boeira <GabryelBoeira@users.noreply.github.com>
2024-12-01 22:01:09 -0300

    Update RecursiveFibonacci.java

--- estruturas-dados-java/src/main/java/com/gabryel/recursividade/RecursiveFibonacci.java
+++ estruturas-dados-java/src/main/java/com/gabryel/recursividade/RecursiveFibonacci.java
@@ -3,20 +3,26 @@ package com.gabryel.recursividade;
 public class RecursiveFibonacci {
 
     /**
-     * The RecursiveFibonacci class provides a method to calculate the nth Fibonacci number
-     * using a recursive approach. The Fibonacci sequence is characterized by the fact that
-     * every number after the first two is the sum of the two preceding ones.
+     * Calculates the nth Fibonacci number using recursion.
+     *
+     * @param n the position in the Fibonacci sequence to calculate
+     * @return the Fibonacci number at the specified position (n)
      */
-    public static int findFibonacci(int n){
-        if (n <= 1)
-            return 1; // CAS0 BASE
+    public static int findFibonacci(int n) {
+        try {
+            if (n <= 1)
+                return 1; // CAS0 BASE
 
-        // PASSO RECURSIVO
-        return findFibonacci(n-1) + findFibonacci(n-2);
+            // PASSO RECURSIVO
+            return findFibonacci(n-1) + findFibonacci(n-2);
+        } catch (StackOverflowError e) {
+            System.out.println("Stack overflow error: Too big reported value to calculate");
+            return 0;
+        }
     }
 
     public static void main(String[] args) {
         // 1, 1, 2, 3, 5, 8, 13
-        System.out.println("Fib(3)= " + findFibonacci(100));
+        System.out.println("Fib(3)= " + findFibonacci(50));
     }
 }
