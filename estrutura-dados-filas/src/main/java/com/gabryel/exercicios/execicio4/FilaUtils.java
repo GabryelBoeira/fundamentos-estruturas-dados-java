c2de53fa6a114f6b35bbefa8fc4a9c46c67278ef
Gabryel Boeira <GabryelBoeira@users.noreply.github.com>
2024-12-09 16:38:38 -0300

    exercicio 4 - filas
     Usando apenas os métodos usuais da fila, escreva um método que remove um valor ‘e’
     fornecido como parâmetro, em um objeto fila também passado como parâmetro.

---
+++ estrutura-dados-filas/src/main/java/com/gabryel/exercicios/execicio4/FilaUtils.java
@@ -0,0 +1,31 @@
+package com.gabryel.exercicios.execicio4;
+
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class FilaUtils {
+
+    /**
+     * Removes the first occurrence of the specified element from the queue.
+     *
+     * This method iterates through the given queue, transferring all elements
+     * except the specified element to a temporary auxiliary queue. Afterward,
+     * it copies the elements back to the original queue. If the specified element
+     * is not found, the queue remains unchanged.
+     *
+     * @param fila the queue from which the element will be removed
+     * @param elemento the element to remove from the queue
+     * @param <T> the type of elements held in the queue
+     */
+    public static <T> void removerElemento(Queue<T> fila, T elemento) {
+        Queue<T> filaAuxiliar = new LinkedList<>();
+
+        while (!fila.isEmpty()) {
+            T valor = fila.poll();
+            if (!valor.equals(elemento))
+                filaAuxiliar.add(valor);
+        }
+        fila.addAll(filaAuxiliar);
+    }
+}
---
+++ estrutura-dados-filas/src/main/java/com/gabryel/exercicios/execicio4/Main.java
@@ -0,0 +1,20 @@
+package com.gabryel.exercicios.execicio4;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+import static com.gabryel.exercicios.execicio4.FilaUtils.removerElemento;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Queue<Integer> minhaFila = new LinkedList<>();
+        minhaFila.add(1);
+        minhaFila.add(2);
+        minhaFila.add(3);
+
+        removerElemento(minhaFila, 2);
+
+        System.out.println(minhaFila);
+    }
+}
