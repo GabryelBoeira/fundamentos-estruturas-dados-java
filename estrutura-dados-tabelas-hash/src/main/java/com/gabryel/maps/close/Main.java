00c49d7e8544883dab4cca9839a8013a55e9b192
Gabryel Boeira <GabryelBoeira@users.noreply.github.com>
2024-12-14 21:22:27 -0300

    Realizando os teste da implementação

--- estrutura-dados-tabelas-hash/src/main/java/com/gabryel/maps/close/Main.java
+++ estrutura-dados-tabelas-hash/src/main/java/com/gabryel/maps/close/Main.java
@@ -5,8 +5,25 @@ import com.gabryel.maps.Map;
 public class Main {
 
     public static void main(String[] args) {
-        Map<String, Integer> hashMap = new HashMapClose<>();
-        hashMap.put("Gabryel", 1);
-        System.out.println(hashMap.get("Gabryel"));
+        Map<Integer, String> hashMap = new HashMapClose<>();
+        hashMap.put(1,"Tony Stark");
+        hashMap.put(2,"Steve Rogers");
+        hashMap.put(3,"Bruce Banner");
+        hashMap.put(4,"Natasha Romanoff");
+        hashMap.put(5,"Thor");
+        hashMap.put(6, "Lanterna Verde");
+        hashMap.put(7, "Batman");
+        hashMap.put(8, "Mulher Maravilha");
+        hashMap.put(9, "Mulher Gato");
+        hashMap.put(10, "Flash");
+
+        System.out.println("Total size: " + hashMap.size());
+        System.out.println("get(\"Thor\"): " + hashMap.get(5));
+
+        System.out.println("\nContains value Thor : " + hashMap.containsValue("Thor"));
+        System.out.println("Contains Key \"5\": " + hashMap.containsKey(5));
+        System.out.println("Remove Key \"Thor\" 5: " + hashMap.remove(5));
+        System.out.println("Contains Key \"Thor\" 5: " + hashMap.containsKey(5));
+        System.out.println("Total size: " + hashMap.size());
     }
 }
